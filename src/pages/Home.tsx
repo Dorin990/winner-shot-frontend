df489263d5fe02b80d71d8a211d0759f85c0501a
Dorin990 <62753038+Dorin990@users.noreply.github.com>
2021-07-04 14:59:18 +0300

    fixes home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,116 +1,132 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { createStyles, Grid, IconButton, InputAdornment, makeStyles, Divider, TextField, Theme, Button } from "@material-ui/core";
+import {
+	createStyles,
+	Grid,
+	IconButton,
+	InputAdornment,
+	makeStyles,
+	Divider,
+	TextField,
+	Theme,
+	Button,
+} from "@material-ui/core";
 import { useState } from "react";
-import { EmailOutlined, Visibility, VisibilityOff} from '@material-ui/icons';
-// import Visibility from "@material-ui/icons/Visibility";
-// import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import { MailOutline, Visibility, VisibilityOff } from "@material-ui/icons";
+import styled from "styled-components";
+
+const WhiteBorderTextField = styled(TextField)`
+	& label.Mui-focused {
+		color: white;
+	}
+	& .MuiOutlinedInput-root {
+		&.Mui-focused fieldset {
+			border-color: white;
+		}
+	}
+`;
 
 const useStyles = makeStyles((theme: Theme) =>
-createStyles({
-  root: {
-    // direction: 'rtl',
-  },
-  title:{
-    color: 'cornsilk',
-    textAlign: 'center'
-  },
-  gridContainer:{
-    backgroundColor: 'burlywood'
-  },
-  gridItem:{
-    textAlign: 'center'
-  },
-  button:{
-    backgroundColor: 'cornsilk',
-    color:'burlywood'
-  }
-}),
+	createStyles({
+		title: {
+			color: "cornsilk",
+			textAlign: "center",
+		},
+		gridContainer: {
+			backgroundColor: "burlywood",
+		},
+		gridItem: {
+			textAlign: "center",
+		},
+		button: {
+			backgroundColor: "cornsilk",
+			color: "burlywood",
+		},
+	}),
 );
 
 export default function Home() {
-  const { loginWithRedirect } = useAuth0();
-  const [allValues, setAllValues] = useState({
-    email: '',
-    password: '',
-    showPassword: false,
- });
+	const { loginWithRedirect } = useAuth0();
+	const [allValues, setAllValues] = useState({
+		email: "",
+		password: "",
+		showPassword: false,
+	});
 
- const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAllValues({...allValues, [event.target.name]: event.target.value})
- }
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setAllValues({ ...allValues, [event.target.name]: event.target.value });
+	};
 
- const handleClickPasswordVisability = () => {
-    setAllValues({...allValues, showPassword: !allValues.showPassword})
- }
- 
- const classes = useStyles();
+	const handleClickPasswordVisability = () => {
+		setAllValues({ ...allValues, showPassword: !allValues.showPassword });
+	};
 
-  return (
-    <>
-    <Grid container className={classes.root}>
-    <Grid item xs={2} />
-    <Grid  item xs={8} container spacing={3} className={classes.gridContainer}>
-      <Grid item xs={12}>
-        <h1 className={classes.title}> Sign in to Winner Shot!</h1>
-        <Divider />
-      </Grid>
-      <Grid item xs={2} />
-      <Grid item xs={8} className={classes.gridItem}>
-        <TextField
-            id="email"
-            name="email"
-            label="Email"
-            type="text"
-            placeholder="Email"
-            variant="outlined"
-            onChange={handleChange}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <EmailOutlined />
-                </InputAdornment>
-              ),
-            }}
-          />
-      </Grid>
-      <Grid item xs={2} />
+	const classes = useStyles();
 
-      <Grid item xs={2} />
-      <Grid item xs={8} className={classes.gridItem}>
-        <TextField
-            id="password"
-            name="password"
-            label="Password"
-            type= {allValues.showPassword ? "text" : "password"}
-            placeholder="Password"
-            variant="outlined"
-            onChange={handleChange}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start" >
-                  <IconButton onClick={handleClickPasswordVisability}>
-                    {allValues.showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                  
-                </InputAdornment>
-              ),
-            }}
-          />
-      </Grid>
-      <Grid item xs={2} />
-      <Grid item xs={2} />
-      <Grid item xs={8} className={classes.gridItem}>
-        <Button variant="contained" 
-          className={classes.button}
-        // className={classes.margin}
-        onClick={() => loginWithRedirect()}>
-          Log In
-        </Button>
-      </Grid>
-      <Grid item xs={2} />
-    </Grid>  
-    <Grid item xs={2} />
-    </Grid>
-    </>
-  );
+	return (
+		<>
+			<Grid
+				alignItems="center"
+				justify="center"
+				xs={8}
+				container
+				spacing={3}
+				className={classes.gridContainer}
+			>
+				<Grid item xs={12}>
+					<h1 className={classes.title}> הרשמה!</h1>
+					<Divider />
+				</Grid>
+				<Grid item className={classes.gridItem}>
+					<WhiteBorderTextField
+						id="email"
+						name="email"
+						label="אי-מייל"
+						type="text"
+						variant="outlined"
+						onChange={handleChange}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="end">
+									<MailOutline />
+								</InputAdornment>
+							),
+						}}
+					/>
+				</Grid>
+				<Grid item xs={8} className={classes.gridItem}>
+					<WhiteBorderTextField
+						id="password"
+						name="password"
+						label="סיסמא"
+						type={allValues.showPassword ? "text" : "password"}
+						placeholder="סיסמא"
+						variant="outlined"
+						onChange={handleChange}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="end">
+									<IconButton onClick={handleClickPasswordVisability}>
+										{allValues.showPassword ? (
+											<Visibility />
+										) : (
+											<VisibilityOff />
+										)}
+									</IconButton>
+								</InputAdornment>
+							),
+						}}
+					/>
+				</Grid>
+				<Grid item xs={8} className={classes.gridItem}>
+					<Button
+						variant="contained"
+						className={classes.button}
+						onClick={() => loginWithRedirect()}
+					>
+						לכניסה לחצו כאן
+					</Button>
+				</Grid>
+			</Grid>
+		</>
+	);
 }
